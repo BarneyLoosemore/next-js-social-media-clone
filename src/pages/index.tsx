71cbf7ccfe97e256d688137e2795e1c19b2c5c45
BarneyLoosemore <barney.loosemore@pollen.co>
2022-06-29 18:51:12 +0200

    🔨 initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Next-JS Social Media Clone</title>
+      </Head>
+
+      <main>
+        <h1 className="text-3xl">Next JS social media clone</h1>
+      </main>
+
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
